6329909c272aa8a61358ff6c27a97de134d42c8b
hiro21 <hiswing8@gmail.com>
2018-11-06 00:39:44 +0900

    taskControllerのRoute実装をしないように修正。

--- src/main/kotlin/TaskController.kt
+++ src/main/kotlin/TaskController.kt
@@ -5,12 +5,12 @@ import spark.Response
 import spark.Route
 
 class TaskController {
-    fun index() : Route = object : Route {
-        override fun handle(request: Request?, response: Response?): Any =
-                listOf(
-                        Task(1, "test1", false),
-                        Task(2, "test2", false)
-                )
+    fun index() : Route = Route { request, response ->
+        listOf(
+                Task(1, "test1", false),
+                Task(2, "test2", true)
+        )
+
     }
 
 }
