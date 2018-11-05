e286a9a5203b2b6d96e40be95c9489bf81672f53
hiro21 <hiswing8@gmail.com>
2018-11-05 23:19:00 +0900

    mainを新規作成

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,17 @@
+package todolist
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import spark.Spark.get
+
+fun main (args : Array<String>) {
+    val objectMapper = ObjectMapper().registerKotlinModule()
+    get("/tasks") { req, res ->
+        val tasks = listOf(
+                Task(1, "test1", false),
+                Task(2, "test2", true)
+        )
+        objectMapper.writeValueAsString(tasks)
+
+    }
+}
