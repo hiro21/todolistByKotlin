15c1eb5277da3ce5593b65c9c3969e60e67c9090
hiro21 <hiswing8@gmail.com>
2018-11-06 23:23:12 +0900

    固定のダミー値だったコントローラを実装

--- src/main/kotlin/TaskController.kt
+++ src/main/kotlin/TaskController.kt
@@ -1,16 +1,10 @@
 package todolist
 
-import spark.Request
-import spark.Response
 import spark.Route
 
-class TaskController {
+class TaskController(private val taskRepository: TaskRepository) {
     fun index() : Route = Route { request, response ->
-        listOf(
-                Task(1, "test1", false),
-                Task(2, "test2", true)
-        )
-
+        taskRepository.findAll()
     }
 
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -7,7 +7,8 @@ import spark.Spark.get
 fun main (args : Array<String>) {
     val objectMapper = ObjectMapper().registerKotlinModule()
     val jsonTransformer = JsonTransformer(objectMapper)
-    val taskController = TaskController()
+    val taskRepository = TaskRepository()
+    val taskController = TaskController(taskRepository)
 
     get("tasks", taskController.index(), jsonTransformer)
 }
