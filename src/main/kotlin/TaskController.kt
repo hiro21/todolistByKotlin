33b54ec1426f49cfcca0797d5f52f309aa0a2362
hiro21 <hiswing8@gmail.com>
2018-11-12 23:35:58 +0900

    createメソッドの追加

--- src/main/kotlin/TaskController.kt
+++ src/main/kotlin/TaskController.kt
@@ -1,10 +1,26 @@
 package todolist
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import spark.Route
+import spark.Spark.halt
+import java.lang.Exception
 
-class TaskController(private val taskRepository: TaskRepository) {
+class TaskController(private val objectMapper: ObjectMapper,
+                     private val taskRepository: TaskRepository) {
     fun index() : Route = Route { request, response ->
         taskRepository.findAll()
+    }
+
+    fun create(): Route = Route { req, res ->
+        val request: TaskCreateRequest =
+            try {
+                objectMapper.readValue(req.bodyAsBytes(), TaskCreateRequest::class.java)
+            } catch (e: Exception) {
+                throw halt(400)
+            }
+        val task = taskRepository.create(request.content)
+        res.status(201)
+        task
     }
 
 }
