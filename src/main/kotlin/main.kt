e1b3d60081f5463628634765e68aede320e6f1a4
hiro21 <hiswing8@gmail.com>
2018-11-12 23:43:04 +0900

    ルーティングを修正。POSTリクエストに対応

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,12 +3,14 @@ package todolist
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import spark.Spark.get
+import spark.Spark.post
 
 fun main (args : Array<String>) {
     val objectMapper = ObjectMapper().registerKotlinModule()
     val jsonTransformer = JsonTransformer(objectMapper)
     val taskRepository = TaskRepository()
-    val taskController = TaskController(taskRepository)
+    val taskController = TaskController(objectMapper, taskRepository)
 
     get("tasks", taskController.index(), jsonTransformer)
+    post("tasks", taskController.create(), jsonTransformer)
 }
